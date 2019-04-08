acb8c8423c40f84ae32a2ccf922bc643ee520ff5
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-04-08 12:16:46 -0400

    Added an id key for stored images

--- Perception/Utilities/PerceptionImageCollectionKeys.swift
+++ Perception/Utilities/PerceptionImageCollectionKeys.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 struct PerceptionImageCollectionKeys {
+  public static let id = "id"
   public static let orientation = "orientation"
   public static let name = "name"
   public static let urlString = "urlString"
