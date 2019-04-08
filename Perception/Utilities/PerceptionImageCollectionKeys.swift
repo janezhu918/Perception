21a1da13eff0ea784140c59a9376fa362d0bad3f
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-04-08 11:51:18 -0400

    updated new files by deletinig uncommented text at the top

--- Perception/Utilities/PerceptionImageCollectionKeys.swift
+++ Perception/Utilities/PerceptionImageCollectionKeys.swift
@@ -1,9 +1,2 @@
-//
-//  PerceptionImageCollectionKeys.swift
-//  Perception
-//
-//  Created by Elizabeth Peraza  on 4/8/19.
-//  Copyright © 2019 JaneZhu. All rights reserved.
-//
 
 import Foundation
--- Perception/Utilities/PerceptionVideoCollectionKeys.swift
+++ Perception/Utilities/PerceptionVideoCollectionKeys.swift
@@ -1,9 +1,3 @@
-//
-//  PerceptionVideoCollectionKeys.swift
-//  Perception
-//
-//  Created by Elizabeth Peraza  on 4/8/19.
-//  Copyright © 2019 JaneZhu. All rights reserved.
-//
+
 
 import Foundation
