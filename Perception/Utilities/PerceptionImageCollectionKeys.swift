b6fd2593acf4167c2ae1d6458534016cd840d879
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-04-08 12:08:24 -0400

    Updated the PerceptionImage collection keys

--- Perception/Utilities/PerceptionImageCollectionKeys.swift
+++ Perception/Utilities/PerceptionImageCollectionKeys.swift
@@ -1,2 +1,8 @@
-
 import Foundation
+
+struct PerceptionImageCollectionKeys {
+  public static let orientation = "orientation"
+  public static let name = "name"
+  public static let urlString = "urlString"
+  public static let videoURLString = "videoURLString"
+}
