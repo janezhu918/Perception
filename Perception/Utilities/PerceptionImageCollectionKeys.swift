8506df2977b4a21f9d2607a25e3a7f60b5a7c0c3
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-04-08 11:46:57 -0400

    added a file for collection view cell and two files for keys

---
+++ Perception/Utilities/PerceptionImageCollectionKeys.swift
@@ -0,0 +1,9 @@
+//
+//  PerceptionImageCollectionKeys.swift
+//  Perception
+//
+//  Created by Elizabeth Peraza  on 4/8/19.
+//  Copyright © 2019 JaneZhu. All rights reserved.
+//
+
+import Foundation
---
+++ Perception/Views/FavoriteCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteCollectionCell.swift
+//  Perception
+//
+//  Created by Elizabeth Peraza  on 4/8/19.
+//  Copyright © 2019 JaneZhu. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCollectionCell: UICollectionViewCell {
+    
+}
