6662f667b135466c1401571416ceaca37a66e851
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-04-08 11:47:00 -0400

    Updated Perception Image Model with Initializer and properties

--- Perception/Models/PerceptionImage.swift
+++ Perception/Models/PerceptionImage.swift
@@ -1,1 +1,29 @@
 import Foundation
+
+struct PerceptionImage {
+  public enum Orientation: String {
+    case up
+    case down
+    case left
+    case right
+    case upMirrored
+    case downMirrored
+    case leftMirrored
+    case rightMirrored
+  }
+  public let videoURLString: String
+  public let name: String
+  public let urlString: String
+  public let orientation: Orientation
+  
+  init(videoURLString: String,
+       name: String,
+       urlString: String,
+       orientation: Orientation){
+    self.videoURLString = videoURLString
+    self.name = name
+    self.urlString = urlString
+    self.orientation = orientation
+  }
+  
+}
