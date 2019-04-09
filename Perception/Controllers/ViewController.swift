dc7e7797cf4156a909ee73798c369c29928b4c6f
janezhu1618 <janezhu@pursuit.org>
2019-04-09 15:19:00 -0400

    importing expanding menu to vc

--- Perception/Controllers/ViewController.swift
+++ Perception/Controllers/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import SceneKit
 import ARKit
+import ExpandingMenu
 
 class ViewController: UIViewController {
   
@@ -10,6 +11,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     view.addSubview(mainView)
+    addExpandingMenu()
     mainView.sceneView.delegate = self
     mainView.sceneView.showsStatistics = true
   }
@@ -32,6 +34,22 @@ class ViewController: UIViewController {
     super.viewWillDisappear(animated)
     mainView.sceneView.session.pause()
   }
+    
+    private func addExpandingMenu() {
+        let menuButtonSize: CGSize = CGSize(width: 64.0, height: 64.0)
+        let menuButton = ExpandingMenuButton(frame: CGRect(origin: CGPoint.zero, size: menuButtonSize), image: UIImage(named: "job")!, rotatedImage: UIImage(named: "job")!)
+        menuButton.center = CGPoint(x: self.view.bounds.width - 32.0, y: self.view.bounds.height - 72.0)
+        view.addSubview(menuButton)
+        
+        let item1 = ExpandingMenuItem(size: menuButtonSize, title: "Filter", image: UIImage(named: "filter")!, highlightedImage: UIImage(named: "filter")!, backgroundImage: UIImage(named: "filter"), backgroundHighlightedImage: UIImage(named: "filter")) { () -> Void in
+            print("item1 clicked")
+        }
+        let item2 = ExpandingMenuItem(size: menuButtonSize, title: "Filter", image: UIImage(named: "filter")!, highlightedImage: UIImage(named: "filter")!, backgroundImage: UIImage(named: "filter"), backgroundHighlightedImage: UIImage(named: "filter")) { () -> Void in
+            print("item1 clicked")
+        }
+        menuButton.addMenuItems([item1/*, item2, item3, item5*/])
+        view.addSubview(menuButton)
+    }
 
 }
 
