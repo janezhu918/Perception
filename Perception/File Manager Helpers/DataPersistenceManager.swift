478c3beefb2f210291dbfcffa720981ec8b6919c
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-04-15 12:12:41 -0400

    Added File Manager

---
+++ Perception/File Manager Helpers/DataPersistenceManager.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+final class DataPersistenceManager {
+  private init() {}
+  
+  public static func documentsDirectory() -> URL {
+    return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+  }
+  
+  public static func filepathToDocumentsDirectory(filename: String) -> URL {
+    return documentsDirectory().appendingPathComponent(filename)
+  }
+  
+  public static func filepathToTempDirectory(filename: String) -> URL {
+    return FileManager.default.temporaryDirectory.appendingPathComponent(filename)
+  }
+}
