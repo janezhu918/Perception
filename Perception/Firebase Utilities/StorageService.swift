492b8a579720c3cea7ad36f446c9f68c0cb50287
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-04-16 14:27:22 -0400

    Refactored the storage service

--- Perception/Firebase Utilities/StorageService.swift
+++ Perception/Firebase Utilities/StorageService.swift
@@ -9,7 +9,6 @@ enum Result<Value> {
 protocol ImageStorageService {
   func storeImage(data:Data, id:String)
   func deleteImage(image:PerceptionImage)
-  var imageServiceDelgate: ImageStorageServiceDelegate? { get set }
 }
 
 protocol ImageStorageServiceDelegate: AnyObject {
@@ -22,7 +21,6 @@ protocol VideoStorageService {
   func storeVideo(data:Data, id:String)
   func storeVideo(url:URL, id:String)
   func deleteVideo(video:PerceptionVideo)
-  var videoServiceDelegate: VideoStorageServiceDelegate? { get set }
 }
 
 protocol VideoStorageServiceDelegate: AnyObject {
@@ -43,8 +41,6 @@ final class StorageService: ImageStorageService, VideoStorageService {
       }
     }
   }
-  public weak var imageServiceDelgate: ImageStorageServiceDelegate?
-  public weak var videoServiceDelegate: VideoStorageServiceDelegate?
   
   private let rootRef: StorageReference = {
     let storage = Storage.storage()
@@ -58,66 +54,6 @@ final class StorageService: ImageStorageService, VideoStorageService {
   private lazy var videoFolderReference: StorageReference = {
     return rootRef.child("\(Content.video.rawValue)s")
   }()
-  
-  public func deleteImage(image: PerceptionImage) {
-    let fileReference = imageFolderReference.child(image.id)
-    fileReference.delete { (error) in
-      guard error == nil else {
-        self.imageServiceDelgate?.storageService(self, didReceiveError: error!)
-        return
-      }
-    }
-    imageServiceDelgate?.storageService(self, didDeleteImage: true)
-  }
-  
-  public func storeImage(data: Data, id:String) {
-    let fileReference = imageFolderReference.child("\(id).png")
-    storeContentData(content: .image, data: data,
-                     fileReference: fileReference) { (result) in
-                      switch result {
-                      case .failure(error: let error):
-                        self.imageServiceDelgate?.storageService(self, didReceiveError: error)
-                      case .success(let url):
-                        self.imageServiceDelgate?.storageService(self, didReceiveImageURL: url)
-                      }
-    }
-  }
-  
-  public func storeVideo(data: Data, id: String) {
-    let fileReference = videoFolderReference.child("\(id).mp4")
-    storeContentData(content: .image, data: data,
-                     fileReference: fileReference) { (result) in
-                      switch result {
-                      case .failure(error: let error):
-                        self.videoServiceDelegate?.storageService(self, didReceiveError: error)
-                      case .success(let url):
-                        self.videoServiceDelegate?.storageService(self, didReceiveVideoURL: url)
-                      }
-    }
-  }
-  
-  public func storeVideo(url: URL, id: String) {
-    let fileReference = videoFolderReference.child("\(id).mp4")
-    storeContentData(content: .image, url: url,
-                     fileReference: fileReference) { (result) in
-                      switch result {
-                      case .failure(error: let error):
-                        self.videoServiceDelegate?.storageService(self, didReceiveError: error)
-                      case .success(let url):
-                        self.videoServiceDelegate?.storageService(self, didReceiveVideoURL: url)
-                      }
-    }
-  }
-  
-  public func deleteVideo(video: PerceptionVideo) {
-    videoFolderReference.child(video.id).delete { (error) in
-      guard error == nil else {
-        self.videoServiceDelegate?.storageService(self, didReceiveError: error!)
-        return
-      }
-      self.videoServiceDelegate?.storageService(self, didDeleteVideo: true)
-    }
-  }
   
   private func storeContentData(content:Content, data:Data,
                                 fileReference:StorageReference,
@@ -167,4 +103,68 @@ final class StorageService: ImageStorageService, VideoStorageService {
     }
   }
   
+}
+
+extension StorageService {
+  public func deleteImage(image: PerceptionImage, completion:@escaping(Result<Bool>) -> Void) {
+    let fileReference = imageFolderReference.child(image.id)
+    fileReference.delete { (error) in
+      guard error == nil else {
+        completion(.failure(error: error!))
+        return
+      }
+    }
+    completion(.success(true))
+  }
+  
+  public func storeImage(data: Data, id:String, completion:@escaping(Result<URL>) -> Void) {
+    let fileReference = imageFolderReference.child("\(id).png")
+    storeContentData(content: .image, data: data,
+                     fileReference: fileReference) { (result) in
+                      switch result {
+                      case .failure(error: let error):
+                        completion(.failure(error: error))
+                      case .success(let url):
+                        completion(.success(url))
+                      }
+    }
+  }
+}
+
+extension StorageService {
+  public func storeVideo(data: Data, id: String, completion:@escaping(Result<URL>) -> Void) {
+    let fileReference = videoFolderReference.child("\(id).mp4")
+    storeContentData(content: .image, data: data,
+                     fileReference: fileReference) { (result) in
+                      switch result {
+                      case .failure(error: let error):
+                        completion(.failure(error: error))
+                      case .success(let url):
+                        completion(.success(url))
+                      }
+    }
+  }
+  
+  public func storeVideo(url: URL, id: String,  completion:@escaping(Result<URL>) -> Void) {
+    let fileReference = videoFolderReference.child("\(id).mp4")
+    storeContentData(content: .image, url: url,
+                     fileReference: fileReference) { (result) in
+                      switch result {
+                      case .failure(error: let error):
+                        completion(.failure(error: error))
+                      case .success(let url):
+                        completion(.success(url))
+                      }
+    }
+  }
+  
+  public func deleteVideo(video: PerceptionVideo, completion:@escaping(Result<Bool>) -> Void) {
+    videoFolderReference.child(video.id).delete { (error) in
+      guard error == nil else {
+        completion(.failure(error: error!))
+        return
+      }
+      completion(.success(true))
+    }
+  }
 }
