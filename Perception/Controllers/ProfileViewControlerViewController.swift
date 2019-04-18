16fab9d6769ddef55dbb4316f66d01323b3475f9
Olimpia <olimpiacompean@prusuit.org>
2019-04-18 01:28:20 -0400

    profile vc setup

--- Perception/Controllers/ProfileViewControlerViewController.swift
+++ Perception/Controllers/ProfileViewControlerViewController.swift
@@ -1,28 +1,46 @@
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
 
 class ProfileViewControlerViewController: UIViewController {
     
     let profileView = ProfileView()
     private var ussersession = AppDelegate.authservice
-    
+    private var datePicker: UIDatePicker?
 
     override func viewDidLoad() {
         super.viewDidLoad()
          view.addSubview(profileView)
         profileView.nameTextField.delegate = self
-    //    ussersession = (UIApplication.shared.delegate as! AppDelegate).usersession
         ussersession.authserviceSignOutDelegate = self
         setupUI()
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+        profileView.agePicker.inputView = datePicker
         
-        guard let user = ussersession.getCurrentUser() else {
-            profileView.userEmail.text = "no logged user"
+       guard let user = ussersession.getCurrentUser() else {
+            profileView.userEmailTextField.text = "no logged user"
             return
         }
-        
-        profileView.userEmail.text = user.email
-        
+        let button1 = UIBarButtonItem(title: "Save changes", style: .plain, target: self, action: #selector(savePreferences))
+       
+        self.navigationItem.rightBarButtonItem  = button1
+        profileView.userEmailTextField.text = user.email
+         keyboardDismiss()
+    }
+    
+    @objc func dateChanged() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        profileView.agePicker.text = dateFormatter.string(from: datePicker!.date)
+      
+    }
+    
+    @objc func savePreferences() {
+        //TODO: guard against the user state
+     
     }
     
     private func setupUI() {
@@ -48,3 +66,13 @@ extension ProfileViewControlerViewController: AuthServiceSignOutDelegate {
 }
 
 
+extension UIViewController {
+    func keyboardDismiss() {
+        let tap: UITapGestureRecognizer =     UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboardS))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboardS() {
+        view.endEditing(true)
+    }
+}
