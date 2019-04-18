d54ef049ed42595f5d75aed87b08aa18158fde14
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-04-18 10:54:09 -0400

    began working on detailed view

--- Perception/Views/VideoDetailView.swift
+++ Perception/Views/VideoDetailView.swift
@@ -9,6 +9,7 @@ class VideoDetailView: UIView {
   }
   public lazy var playerView: VideoView = {
     return VideoView()
+    
   }()
   
   public lazy var descriptionView: UITextView = {
@@ -25,6 +26,7 @@ class VideoDetailView: UIView {
   override init(frame: CGRect) {
     super.init(frame: UIScreen.main.bounds)
     commonInit()
+    setConstraints()
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -36,31 +38,57 @@ class VideoDetailView: UIView {
     setConstraints()
   }
   
-  private func setConstraints() {
-    addSubview(playerView)
-    addSubview(nameLabel)
-    addSubview(descriptionView)
-    
-    playerView.translatesAutoresizingMaskIntoConstraints = false
-    descriptionView.translatesAutoresizingMaskIntoConstraints = false
-    nameLabel.translatesAutoresizingMaskIntoConstraints = false
-    
-    playerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: UI.heightMargin).isActive = true
-    playerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
-    playerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-    playerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: UI.videoHeight).isActive = true
-    
-    nameLabel.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: UI.heightMargin).isActive = true
-    nameLabel.widthAnchor.constraint(equalTo: playerView.widthAnchor, constant: UI.width).isActive = true
-    nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-    
-    descriptionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11).isActive = true
-    descriptionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
-    descriptionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-  }
   
   public func configurePlayer(player:AVPlayer){
     playerView.player = player
   }
 
 }
+
+extension VideoDetailView {
+  
+  private func setConstraints() {
+    setPlayerConstraints()
+//    setNameLabelConstraints()
+//    setDescriptionConstraints()
+  }
+  
+  private func setPlayerConstraints() {
+    addSubview(playerView)
+    playerView.translatesAutoresizingMaskIntoConstraints = false
+    playerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 11).isActive = true
+    playerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+    playerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+    playerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    playerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
+    playerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: UI.videoHeight).isActive = true
+
+
+  }
+  
+  private func setNameLabelConstraints() {
+    addSubview(nameLabel)
+    nameLabel.translatesAutoresizingMaskIntoConstraints = false
+
+  }
+  
+  private func setDescriptionConstraints() {
+    addSubview(descriptionView)
+    descriptionView.translatesAutoresizingMaskIntoConstraints = false
+
+  }
+  
+}
+
+
+//playerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: UI.heightMargin).isActive = true
+//playerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//
+//nameLabel.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: UI.heightMargin).isActive = true
+//nameLabel.widthAnchor.constraint(equalTo: playerView.widthAnchor, constant: UI.width).isActive = true
+//nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+//
+//descriptionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11).isActive = true
+//descriptionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
+//descriptionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+//  
