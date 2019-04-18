97e8cf7893331ab881596fda5bcf4a1d257a9cd4
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-04-18 12:47:24 -0400

    modified constrains

--- Perception/Views/VideoDetailView.swift
+++ Perception/Views/VideoDetailView.swift
@@ -1,32 +1,51 @@
 import UIKit
 import AVFoundation
 
+
+private struct UI {
+  public static let heightMargin: CGFloat = 11
+  public static let width: CGFloat = 0.9
+  public static let videoHeight: CGFloat = 0.4
+}
+
 class VideoDetailView: UIView {
-  private struct UI {
-    public static let heightMargin: CGFloat = 11
-    public static let width: CGFloat = 0.9
-    public static let videoHeight: CGFloat = 0.4
-  }
+
   public lazy var playerView: VideoView = {
     return VideoView()
     
   }()
   
-  public lazy var descriptionView: UITextView = {
-    let tv = UITextView()
-    tv.isEditable = false
-    return tv
-  }()
   
   public lazy var nameLabel: UILabel = {
     let lbl = UILabel()
+    lbl.text = "VIDEO NAME"
+    lbl.backgroundColor = #colorLiteral(red: 0.1490196078, green: 0.2, blue: 0.3294117647, alpha: 1)
+    lbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+//    lbl.layer.cornerRadius = 3.0
+//    lbl.layer.borderWidth = 1.0
+//    lbl.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    lbl.font = UIFont(name: "STHeitiTC-Medium", size: 32)
     return lbl
   }()
   
+  public lazy var descriptionView: UITextView = {
+    let tv = UITextView()
+    tv.isEditable = false
+    tv.text = "This movie is awesome because..."
+    tv.font = UIFont(name: "STHeitiTC-Medium", size: 16)
+    tv.backgroundColor = #colorLiteral(red: 0.1490196078, green: 0.2, blue: 0.3294117647, alpha: 1)
+    tv.textColor = #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
+    tv.layer.cornerRadius = 3.0
+    tv.layer.borderWidth = 1.0
+    tv.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    tv.isEditable = false
+    return tv
+  }()
+ 
   override init(frame: CGRect) {
     super.init(frame: UIScreen.main.bounds)
-    commonInit()
     setConstraints()
+    commonInit()
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -34,7 +53,7 @@ class VideoDetailView: UIView {
   }
   
   private func commonInit() {
-    backgroundColor = #colorLiteral(red: 0.1276455522, green: 0.2034990788, blue: 0.3436715901, alpha: 1)
+    backgroundColor = #colorLiteral(red: 0.1347674131, green: 0.2018641531, blue: 0.3395293951, alpha: 1)
     setConstraints()
   }
   
@@ -49,46 +68,36 @@ extension VideoDetailView {
   
   private func setConstraints() {
     setPlayerConstraints()
-//    setNameLabelConstraints()
-//    setDescriptionConstraints()
+    setNameLabelConstraints()
+    setDescriptionConstraints()
   }
   
   private func setPlayerConstraints() {
     addSubview(playerView)
     playerView.translatesAutoresizingMaskIntoConstraints = false
+    
     playerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 11).isActive = true
-    playerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-    playerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-    playerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-    playerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
-    playerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: UI.videoHeight).isActive = true
-
-
+    playerView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 11).isActive = true
+    playerView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -11).isActive = true
+    playerView.heightAnchor.constraint(equalToConstant: 30).isActive = true
   }
   
   private func setNameLabelConstraints() {
     addSubview(nameLabel)
     nameLabel.translatesAutoresizingMaskIntoConstraints = false
-
+    nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 11).isActive = true
+    nameLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -11).isActive = true
+    nameLabel.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor).isActive = true
+    nameLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
   }
   
   private func setDescriptionConstraints() {
     addSubview(descriptionView)
     descriptionView.translatesAutoresizingMaskIntoConstraints = false
-
+    descriptionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11).isActive = true
+    descriptionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 11).isActive = true
+    descriptionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -11).isActive = true
+    descriptionView.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.35).isActive = true
   }
   
 }
-
-
-//playerView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: UI.heightMargin).isActive = true
-//playerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-//
-//nameLabel.topAnchor.constraint(equalTo: playerView.bottomAnchor, constant: UI.heightMargin).isActive = true
-//nameLabel.widthAnchor.constraint(equalTo: playerView.widthAnchor, constant: UI.width).isActive = true
-//nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-//
-//descriptionView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11).isActive = true
-//descriptionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: UI.width).isActive = true
-//descriptionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-//  
