acbaefc62e71a5d8a1a60b0657e1bf4511e2c00b
Olimpia1988 <43769680+Olimpia1988@users.noreply.github.com>
2019-04-30 13:02:20 -0400

    Animations (#69)
    * rotation file added
    
    * rotation fully setup

--- Perception/Controllers/ProfileViewControlerViewController.swift
+++ Perception/Controllers/ProfileViewControlerViewController.swift
@@ -48,6 +48,22 @@ class ProfileViewControlerViewController: UIViewController {
       
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        AppUtility.lockOrientation(.portrait)
+        // Or to rotate and lock
+         AppUtility.lockOrientation(.portrait, andRotateTo: .portrait)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppUtility.lockOrientation(.all)
+    }
+    
   
     override var shouldAutorotate: Bool {
         return false 
