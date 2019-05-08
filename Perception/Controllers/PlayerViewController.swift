ccbf82a166b93712d0227724c927831f36c13b86
iosdevrookie <dev@iosdevrookies-MacBook-Pro.local>
2019-05-08 18:05:45 -0400

    Added a Player View Controller

--- Perception/Controllers/PlayerViewController.swift
+++ Perception/Controllers/PlayerViewController.swift
@@ -1,10 +1,27 @@
 import UIKit
 import AVKit
 class PlayerViewController: AVPlayerViewController {
-//    var player: AVPlayer?
+  
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+      super.viewDidLoad()
+    }
+  
+    override func viewWillDisappear(_ animated: Bool) {
+      super.viewWillDisappear(animated)
+      player?.pause()
+    }
+  
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+    }
+  
+    init(player: AVPlayer) {
+      super.init(nibName: nil, bundle: nil)
+      self.player = player
+    }
+  
+    deinit {
+      player?.pause()
     }
 
 }
